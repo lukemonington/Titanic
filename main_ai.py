490b10efcd9fc77884da5b7831f587003d6e8ef0
Luke Monington <lukemonington3@gmail.com>
2020-08-19 09:42:29 -0600

    Created main_ai file
    Created main_ai file and then wrote the code to import the data into the train and test dataset

---
+++ main_ai.py
@@ -0,0 +1,10 @@
+import pandas as pd
+pd.set_option('max_columns', None)
+import numpy as np
+
+
+# Get train and test dataset
+train = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Titanic\train.csv')
+test = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Titanic\test.csv')
+
+train.head()
