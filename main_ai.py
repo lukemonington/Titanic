214f0235c2f5fce290dcf5f452a28c83ab147f63
Luke Monington <lukemonington3@gmail.com>
2020-08-24 07:57:32 -0600

    Initial changes

--- main_ai.py
+++ main_ai.py
@@ -2,9 +2,11 @@ import pandas as pd
 pd.set_option('max_columns', None)
 import numpy as np
 from sklearn.model_selection import train_test_split
-import xgboost
+import xgboost as xgb
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
 # Get train and test dataset
@@ -13,90 +15,52 @@ test = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Titanic\test.csv'
 example_submission = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Titanic\gender_submission.csv')
 PassIDs = test['PassengerId']
 
-#this is in the Initial-EDA branch
+target = train['Survived']
+train = train.drop('Survived', axis = 1)
+
+train = train.fillna(-999)
+test = test.fillna(-999)
+
+X_train = train
+X_test = test
+y_train = target
+
+# For loop to encode all columns with "object" datatype
+for f in X_train.columns:
+    if X_train[f].dtype == "object" or X_test[f].dtype == "object":
+        lbl = preprocessing.LabelEncoder()
+        lbl.fit(list(X_train[f].values) + list(X_test[f].values))
+        X_train[f] = lbl.transform(list(X_train[f].values))
+        X_test[f] = lbl.transform(list(X_test[f].values))
+
+X = X_train
+y = y_train
+test = X_test
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size = 0.2)
+
+def get_mae(max_leaf_nodes, X_train, X_val, y_train, y_val):
+    
 
 
-train.dtypes
-# from this we can see that PassengerId, Survived, Pclass, SibSp, and Parch are dtype int64
-# Fare is dtype float64
-# Name, Sex, Ticket, Cabin, and Embarked are dtype object
 
-train.isnull().sum()
-# From this we can see that Cabin has 687 null values, Age has 177, and Embarked has 2
-# Since Cabin has so many null values, I'm just going to remove that column
-train = train.drop(['Cabin'], axis = 1)
-test = test.drop(['Cabin'], axis = 1)
+clf = xgb.XGBClassifier(
+    n_estimators=500,
+    max_depth=9,
+    learning_rate=0.05,
+    subsample=0.9,
+    colsample_bytree=0.9,
+    missing=-999,
+    random_state=2019,
+    tree_method='gpu_hist'  # THE MAGICAL PARAMETER
+)
+%time clf.fit(X_train, y_train)
 
-train.shape
 
-# Replace the null age values with 21
-train['Age'] = train['Age'].fillna(21)
-test['Age'] = test['Age'].fillna(21)
+pred = clf.predict(X_test)
 
-# Replace the null Embarked values with S
-train['Embarked'] = train['Embarked'].fillna('S')
-train['Embarked'] = train['Embarked'].fillna('S')
 
-# Replace the one null Fare value with 7
-test['Fare'] = test['Fare'].fillna(7)
-
-# Drop the name column, since I don't think name will have any useful information
-train = train.drop(['Name'], axis = 1)
-test = test.drop(['Name'], axis = 1)
-
-# For the Sex class, I'm going to change this into a binary class, male = 1, female = 0
-sex_dictionary = {'female':0, 'male':1}
-train['Sex'] = train['Sex'].map(sex_dictionary)
-test['Sex'] = test['Sex'].map(sex_dictionary)
-
-# For the embarked column, there seems to be two rows with a value of "", replace that with "S" (most common)
-print(train['Embarked'].value_counts().sort_values())
-train['Embarked'] = train['Embarked'].replace("", "S")
-print(test['Embarked'].value_counts().sort_values())
-
-train.shape
-# Train shape = (891, 10)
-test.shape
-# Test shape = 418, 9
-
-target_df = train['Survived']
-train = train.drop(['Survived'], axis = 1)
-
-train_test_list = [train,test]
-train_test_df = pd.concat(train_test_list, axis = 0)
-
-train_test_dummies = train_test_df['Embarked']
-train_test_df = train_test_df.drop(['Embarked'], axis = 1)
-
-dummy_df = pd.get_dummies(train_test_dummies, sparse = False)
-dummies_list = [train_test_df, dummy_df]
-train_test_df = pd.concat(dummies_list, axis = 1)
-
-train = train_test_df.iloc[0:891,:]
-test = train_test_df.iloc[891:,:]
-
-train = train.drop(['Ticket'], axis = 1)
-test = test.drop(['Ticket'], axis = 1)
-seed = 7
-test_size = 0.2
-X_train, X_test, y_train, y_test = train_test_split(train, target_df, test_size=test_size, random_state=seed)
-
-# Using data to train AI
-model = XGBClassifier(
-    learning_rate = 0.1,
-    n_estimators = 75,
-    max_depth = 3,
-    min_child_weight = 0.5)
-
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
-
-pred = model.predict(test)
-# Creating submission csv file
 submission = pd.DataFrame(PassIDs, columns = ['PassengerId'])
 submission['Survived'] = pred
-path = r"C:\Users\lukem\Desktop\Github AI Projects\Titanic\submissions\titanic_submission_1.csv"
-submission.to_csv(path, index = False)
+path = r"C:\Users\lukem\Desktop\Github AI Projects\Submissions\Titanic\ "
+submission.to_csv(path + "submissionv1.csv", index = False)
