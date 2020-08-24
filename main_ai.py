ace93bd24782e2704b0bef3089e66e5e1cfd0027
Luke Monington <lukemonington3@gmail.com>
2020-08-24 12:40:58 -0600

    tried out a get_mae function

--- main_ai.py
+++ main_ai.py
@@ -9,6 +9,8 @@ from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.metrics import mean_absolute_error
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
 
 
 # Get train and test dataset
@@ -42,8 +44,39 @@ test = X_test
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size = 0.2)
 
 def get_mae(max_leaf_nodes, X_train, X_val, y_train, y_val):
-    
+    dtr = DecisionTreeRegressor(max_leaf_nodes = max_leaf_nodes)
+    dtr.fit(X_train, y_train)
+    preds = dtr.predict(X_val)
+    mae = mean_absolute_error(preds, y_val)
+    return(mae)
 
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+model = DecisionTreeRegressor(max_leaf_nodes=20, random_state=0)
+model.fit(X_train, y_train)
+preds_val = model.predict(X_val)
+mae = mean_absolute_error(y_val, preds_val)
+print(mae)
+
+train_X = X_train
+train_y = y_train
+val_X = X_val
+val_y = y_val
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
+
+leafs_to_test = [2, 20, 50, 500]
+
+for i in leafs_to_test:
+    my_mae = get_mae(i, X_train, X_val, y_train, y_val)
+    print("max_leaf_nodes: %d, mean absolute error: %d" %(i, my_mae))
 
 
 clf = xgb.XGBClassifier(
