cb3f925cbce8a1813cee4cfddfa53204ba753d79
Luke Monington <lukemonington3@gmail.com>
2020-08-29 10:00:23 -0600

    Doing a heatmap on the confusion matrix

--- other_ai.py
+++ other_ai.py
@@ -6,6 +6,7 @@ import lightgbm as lgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 
 train = pd.read_csv(r'C:\Users\lukem\Desktop\Github AI Projects\Titanic\train.csv')
@@ -57,11 +58,17 @@ print("The model is {} and the accuracy is {:+}".format("lgb", acc*100)) # Adds 
 print("The model is {0} and the accuracy is {1:.4f}%".format("lgb",acc*100))
 
 cm = confusion_matrix(y_test, y_pred)
-print(cm)
+print("The model is {0} and the accuracy is {1:.4f}%".format("lgb",acc*100))
+#print(cm)
 print("The number of true positives is {}".format(cm[0,0]))
 print("The number of false positives is {}".format(cm[0,1]))
 print("The number of true negatives is {}".format(cm[1,1]))
 print("The number of false negatives is {}".format(cm[1,0]))
+
+# visualize confusion matrix with seaborn heatmap
+cm_matrix = pd.DataFrame(data=cm, columns=['Actual Positive:1', 'Actual Negative:0'], 
+                                 index=['Predict Positive:1', 'Predict Negative:0'])
+sns.heatmap(cm_matrix, annot=True, fmt='d', cmap='YlGnBu')
 
 
 clf.fit(X, y)
